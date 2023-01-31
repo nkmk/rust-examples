38c6e8501d931bcbdf341b30998abfaebf967a38
nkmk <nkmk.on@gmail.com>
2023-01-31 21:40:37 +0900

    add numeric literals suffix

---
+++ src/bin/numeric_literals_suffix.rs
@@ -0,0 +1,11 @@
+#[allow(unused_variables)]
+fn main() {
+    let i: i32 = 100;
+    let i = 100i32;
+
+    let f: f64 = 0.123;
+    let f = 0.123f64;
+
+    let i = 100_i32;
+    let f = 0.123_f64;
+}
